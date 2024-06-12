5629b4421f92a6fb7f134b7fb90b7e3d47b2a413
ng-srikanth <srikanth.b@ngenux.com>
2024-06-12 12:01:38 +0530

    added yml file

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,3 @@
-# import streamlit as st
-# from download import download_youtube_video
-
-# with st.sidebar:
-#     url_input = st.text_input(label = "paste url")
-#     uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov", "mkv"])
-#     get_video_button = st.button("Get Video")
-
-# output_path = "./downloads"
-# if get_video_button:
-#     video_file_name = download_youtube_video(url_input,output_path)
-#     video_file = open(f'C:/Users/SrikanthBachannagari/Documents/Projects/video-to-notes/downloads/{video_file_name}', 'rb')
-#     video_bytes = video_file.read()
-
-# st.video(video_bytes)
-
 import streamlit as st
 from download import download_youtube_video
 import boto3
