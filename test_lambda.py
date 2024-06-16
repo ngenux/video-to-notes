c84f4ba49f07b86927cc8e3f9522e08e2f3ec4b1
ng-srikanth <srikanth.b@ngenux.com>
2024-06-17 00:58:38 +0530

    updated test file

--- test_lambda.py
+++ test_lambda.py
@@ -124,15 +124,16 @@ class TestLambdaFunction(unittest.TestCase):
     @patch('boto3.client')
     def test_initialize_s3_client(self, mock_boto_client):
         # Patch environment variables
-        with patch.dict(os.environ, {'KEY': 'test_key', 'S_KEY': 'test_secret'}):
-            client = initialize_s3_client()
+        os.environ['AWS_ACCESS_KEY_ID'] = 'test_key'
+        os.environ['AWS_SECRET_ACCESS_KEY'] = 'test_secret'
+        client = initialize_s3_client()
 
-            mock_boto_client.assert_called_with(
-                's3',
-                aws_access_key_id='test_key',
-                aws_secret_access_key='test_secret'
-            )
-            self.assertEqual(client, mock_boto_client.return_value)
+        mock_boto_client.assert_called_with(
+            's3',
+            aws_access_key_id='test_key',
+            aws_secret_access_key='test_secret'
+        )
+        self.assertEqual(client, mock_boto_client.return_value)
 
 if __name__ == '__main__':
     unittest.main()
