a5c844c762aee8b706e844682d31fdc9cf4db38c
ng-srikanth <srikanth.b@ngenux.com>
2024-06-17 01:03:54 +0530

    updated test file

--- test_lambda.py
+++ test_lambda.py
@@ -117,7 +117,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 import boto3
 import os
-from lambdafunction import initialize_s3_client
+from lambda_function import initialize_s3_client
 
 class TestLambdaFunction(unittest.TestCase):
 
